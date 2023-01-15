a4b0d6ee3a9b5b5d1f9bac4b0d37f0947dcfd821
isalvaus <116349441+isalvaus@users.noreply.github.com>
2023-01-16 00:26:57 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,14 +9,47 @@ input.onLogoEvent(TouchButtonEvent.Pressed,()=>{
 })
 
 let brillo = 0
-basic.forever(() => {
-    let newBrillo = pins.P2.analogRead()
-    if (Math.abs(newBrillo - brillo) > 1){
-        //basic.showNumber(newBrillo)
-        //serial.writeValue("Brillo", newBrillo)
-        radio.sendValue("LUZ", newBrillo)
+let temp = 0
 
-        brillo = newBrillo
+basic.forever(() => {
+
+    switch(service){
+        case Service.Luz:
+
+        let newBrillo = pins.P2.analogRead()
+        if (Math.abs(newBrillo - brillo) > 1){
+            //basic.showNumber(newBrillo)
+            //serial.writeValue("Brillo", newBrillo)
+            radio.sendValue("LUZ", newBrillo)
+
+            brillo = newBrillo
+        }
+
+        break;
+
+        case Service.Lock:
+
+        if (input.buttonIsPressed(Button.A))
+            radio.sendValue("LOCK", 1)
+        else if (input.buttonIsPressed(Button.B))
+            radio.sendValue("LOCK", 0)
+
+        break;
+
+        case Service.Termostato:
+
+            let newTemp = pins.P2.analogRead()
+            if (Math.abs(newTemp - temp) > 1) {
+                //basic.showNumber(newBrillo)
+                //serial.writeValue("Brillo", newBrillo)
+                radio.sendValue("TEMP", newTemp)
+
+                temp = newTemp
+            }
+
+        
+
+    
     }
  
  })
